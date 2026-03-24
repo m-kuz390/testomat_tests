a5e1105fe99e5a0c18cab9cc28664cc7b0522870
m.kuz <m.kuz390@gmail.com>
2026-03-25 00:50:43 +0200

    MNFCT-2 added dotenv library for configurations added dataclass library for simplification of configs added library faker for random data generation

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,22 +1,37 @@
+import os
+
+import pytest
+from faker import Faker
 from playwright.sync_api import Page, expect
 
+from tests.conftest import Config
 
-def test_login_with_invalid_creds(page: Page):
+TARGET_PROJECT = "Grocery, Outdoors & Shoes"
+
+
+@pytest.fixture(scope="function")
+def login(page: Page, configs: Config):
+    page.goto(configs.login_url)
+    login_user(page, configs.email, configs.password)
+
+
+def test_login_with_invalid_creds(page: Page, configs):
     open_home_page(page)
     expect(page.locator("[href*='sign_in'].login-item")).to_be_visible()
 
+    invalid_password = Faker().password(length=10)
 
     open_login_form(page)
-    login_user(page, "m.kuz390@gmail.com", "Zaqq1234567890!")
+    login_user(page, configs.email, invalid_password)
 
-    expect(page.locator("#content-desktop").get_by_text("Invalid Email or password")).to_be_visible()
-    expect(page.locator("#content-desktop .common-flash-info")).to_have_text("Invalid Email or password.")
+    expect(page.locator("#content-desktop").get_by_text("Invalid email or password")).to_be_visible()
+    expect(page.locator("#content-desktop .common-flash-info")).to_have_text("Invalid email or password.")
 
 
-def test_login_success(page: Page):
+def test_login_success(page: Page, configs: Config):
     open_home_page(page)
     open_login_form(page)
-    login_user(page, "m.kuz390@gmail.com", "qwerty")
+    login_user(page, configs.email, configs.password)
 
     expect(page).to_have_url("https://app.testomat.io/")
     expect(page.locator(".common-flash-success").get_by_text("Signed in successfully")).to_be_visible()
@@ -32,10 +47,10 @@ def test_start_free_trial(page: Page):
     expect(page).to_have_url("https://app.testomat.io/users/sign_up")
 
 
-def test_open_project(page: Page):
+def test_open_project(page: Page, configs: Config):
     open_home_page(page)
     open_login_form(page)
-    login_user(page, "m.kuz390@gmail.com", "qwerty")
+    login_user(page, configs.email, configs.password)
 
     expect(page.locator("#company_id")).to_contain_text("QA Club Lviv")
 
@@ -45,10 +60,10 @@ def test_open_project(page: Page):
     expect(page).to_have_url("https://app.testomat.io/projects/grocery-outdoors-shoes/")
 
 
-def test_open_create_project(page: Page):
+def test_open_create_project(page: Page, configs: Config):
     open_home_page(page)
     open_login_form(page)
-    login_user(page, "m.kuz390@gmail.com", "qwerty")
+    login_user(page, configs.email, configs.password)
 
     page.locator("#company_id").select_option(label="Free Projects")
 
@@ -59,34 +74,24 @@ def test_open_create_project(page: Page):
     expect(page).to_have_url("https://app.testomat.io/projects/new")
 
 
-def test_search_project_in_company(page: Page):
-    page.goto("https://app.testomat.io/users/sign_in")
+def test_search_project_in_company(page: Page, login):
+    search_for_project(page, TARGET_PROJECT)
 
-    login_user(page, "m.kuz390@gmail.com", "qwerty")
-
-    target_project = "Grocery, Outdoors & Shoes"
-
-    search_for_project(page, target_project)
-
-    expect(page.get_by_role("heading", name=target_project)).to_be_visible()
+    expect(page.get_by_role("heading", name=TARGET_PROJECT)).to_be_visible()
 
 
-def test_should_be_possible_to_open_free_project(page: Page):
-    page.goto("https://app.testomat.io/users/sign_in")
-    login_user(page, "m.kuz390@gmail.com", "qwerty")
-
+def test_should_be_possible_to_open_free_project(page: Page, login):
     page.locator("#company_id").click()
     page.locator("#company_id").select_option("Free Projects")
 
-    target_project = "Grocery, Outdoors & Shoes"
-    search_for_project(page, target_project)
-    expect(page.get_by_role("heading", name=target_project)).to_be_hidden()
+    search_for_project(page, TARGET_PROJECT)
+    expect(page.get_by_role("heading", name=TARGET_PROJECT)).to_be_hidden()
 
     expect(page.get_by_text("You have not created any projects yet")).to_be_visible(timeout=10000)
 
 
 def open_home_page(page: Page):
-    page.goto("https://testomat.io")
+    page.goto(os.getenv("BASE_URL"))
 
 
 def open_login_form(page: Page):
